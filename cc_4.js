106872513292119c267564829760ef7b78fca7a0
Fernando <mhfernando778@gmail.com>
2025-02-02 01:42:24 -0500

    Task 3 - comments and corrections

--- cc_4.js
+++ cc_4.js
@@ -1,11 +1,13 @@
 // Task 1 - Customer Discounts //
-
 let purchaseAmount = 500; // declaring purchase amount to be 500
+let discountedPurchase = purchaseAmount;
+
 if (purchaseAmount > 100) {
-    console.log("Apply Discount");
+    discountedPurchase = purchaseAmount * .95;
 } // applying a discount if amount is greater than 100
 
-console.log(purchaseAmount); // logging the purchase amount and whether a discount applies
+console.log(`Final amount: ${discountedPurchase}`); // logging the purchase amount and whether a discount applies
+
 
 // Task 2 - Sales Report //
 let sales = [100, 50, 120, 200, 150]; // declaring a sales array with 5 different numbers
@@ -17,14 +19,15 @@ for (let i = 0; i < sales.length; i++) {
     totalSales += sales[i];
 } // creating a for loop to add the sales numbers together
 
-console.log("Total Sales:", totalSales); // logging the total amount of sales
+console.log(`Total Sales: ${totalSales}`); // logging the total amount of sales
+
 
 // Task 3 - Inventory Depletion //
-let stock = 10;
+let stock = 10; // declaring a stock amount of 10
 
 while (stock > 0) {
     console.log("Stock count:", stock);
     stock--;
-}
+} // creating a loop to decrease the stock
 
-console.log(`Decreased Stock: ${stock}`);
+console.log(`Depleting Stock: ${stock}`); // logging the decrement of the stock
