7f799557a7ab06e975bedffbe873a68eff2fc20c
Fernando <mhfernando778@gmail.com>
2025-02-02 00:19:24 -0500

    Task 1 - comments

--- cc_4.js
+++ cc_4.js
@@ -1,7 +1,8 @@
 // Task 1 - Customer Discounts //
-let purchaseAmount = 500;
+
+let purchaseAmount = 500; // declaring purchase amount to be 500
 if (purchaseAmount > 100) {
     console.log("Apply Discount");
-}
+} // applying a discount if amount is greater than 100
 
-console.log(purchaseAmount);
+console.log(purchaseAmount); // logging the purchase amount and whether a discount applies
