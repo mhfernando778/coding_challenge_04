cc2620a51ab9d3d15dcd2d39933683e41afd65e5
Fernando <mhfernando778@gmail.com>
2025-02-02 00:47:14 -0500

    Task 4 - comments

--- cc_4.js
+++ cc_4.js
@@ -8,13 +8,13 @@ if (purchaseAmount > 100) {
 console.log(purchaseAmount); // logging the purchase amount and whether a discount applies
 
 // Task 2 - Sales Report //
-let sales = [100, 50, 120, 200, 150];
-console.log("Sales:", sales);
+let sales = [100, 50, 120, 200, 150]; // declaring a sales array with 5 different numbers
+console.log("Sales:", sales); // logging the sales array
 
 let totalSales = 0;
 
 for (let i = 0; i < sales.length; i++) {
     totalSales += sales[i];
-}
+} // creating a for loop to add the sales numbers together
 
-console.log("Total Sales:", totalSales);
+console.log("Total Sales:", totalSales); // logging the total amount of sales
