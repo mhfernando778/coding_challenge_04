41d1655b434518cfd5faadb9c1b27ddafba9fe96
Fernando <mhfernando778@gmail.com>
2025-02-02 00:45:05 -0500

    Task 2 - Sales Report

--- cc_4.js
+++ cc_4.js
@@ -6,3 +6,15 @@ if (purchaseAmount > 100) {
 } // applying a discount if amount is greater than 100
 
 console.log(purchaseAmount); // logging the purchase amount and whether a discount applies
+
+// Task 2 - Sales Report //
+let sales = [100, 50, 120, 200, 150];
+console.log("Sales:", sales);
+
+let totalSales = 0;
+
+for (let i = 0; i < sales.length; i++) {
+    totalSales += sales[i];
+}
+
+console.log("Total Sales:", totalSales);
